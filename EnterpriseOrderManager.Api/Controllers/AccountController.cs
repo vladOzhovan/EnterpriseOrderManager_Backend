c9d64f4d8711e486adef2eb53a87d7bccb1de1c3
Vladimir <vovchik.v.kf@gmail.com>
2025-12-21 07:49:24 +0100

    AppDbContext, AccountController start

---
+++ EnterpriseOrderManager.Api/Controllers/AccountController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EnterpriseOrderManager.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AccountController : ControllerBase
+    {
+        public AccountController()
+        {
+            
+        }
+
+
+    }
+}
